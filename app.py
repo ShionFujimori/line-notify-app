75351e69bd6a52ad09710b82ddb787919dc06bf4
ShionFujimori <shion888@gmail.com>
2021-12-21 19:46:39 -0500

    Add get_search_results_num function

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import requests
 import os
+import time
 import datetime
+import urllib
 from dotenv import load_dotenv
+
+# web crawling
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# web scraping
+from bs4 import BeautifulSoup
 
 
 # Obtain LINE token from .env file
@@ -16,7 +25,7 @@ def send_notification(message: str):
     """
     dt_now = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     token_dict = {'Authorization': f'Bearer {LINE_TOKEN}'}
-    send_dict = {'message': dt_now + message}
+    send_dict = {'message': dt_now + '\n' + message}
 
     post = requests.post(API_URL, headers=token_dict, data=send_dict)
 
@@ -37,6 +46,46 @@ def get_message():
     return message
 
 
+def get_search_results_num(search_words: str, window=False):
+    """ Get a number of search results for `search_words`.
+
+    Args:
+        - window: if True, then a window browser will be opened.
+    """
+    # Initialize Google Chrome
+    # (Need to install ChromeDriver: `brew install chromedriver`)
+    global browser
+    if not window:
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        browser = webdriver.Chrome(options=op)
+    else:
+        browser = webdriver.Chrome()
+
+    # Go to google.com
+    browser.get('https://www.google.com/')
+    time.sleep(2)
+
+    # Type in `search_words` into the textbox
+    elem = browser.find_element(By.NAME, 'q')
+    elem.clear()
+    elem.send_keys(search_words)
+    time.sleep(2)
+    
+    # Click search button
+    button = browser.find_element(By.NAME, 'btnK')
+    button.click()
+
+    # Extract a number of search results
+    result = browser.find_element(By.ID, 'result-stats').text
+
+    return f'"{search_words}": {result}'
+
+
+
+
 if __name__ == '__main__':
-    message = get_message()
+    message = get_search_results_num('aaa')
+    # message = get_message()
+
     send_notification(message)
